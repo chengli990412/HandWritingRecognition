103307dcb066b0c739fa0888185c20c84f201a7d
Cheng先生 <cheng425863@icloud.com>
2023-04-01 19:38:37 +0800

    版本：V1.0.0.3 1.完善并美化UI。

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
 import sys
-from  datetime import datetime
+from datetime import datetime
+
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog
-from UIMain import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QWidget,QMainWindow
+
 from DeepLearning import DeepLearning
+from UIMain import Ui_MainWindow
+
+from qt_material import apply_stylesheet
 
 
-# 版本：1.0.0.2
-# 更新日期：20230332
+class MyClass(QMainWindow, Ui_MainWindow):
+    Ver="版本：1.0.0.3       更新日期：20230401"
 
-
-class MyClass(QWidget, Ui_MainWindow):
     dl = DeepLearning()
-
-    def __init__(self):
-        super(MyClass, self).__init__()
+    def __init__(self,parent=None):
+        super(MyClass, self).__init__(parent=parent)
         self.init()
         self.dl.textWritten.connect(self.write_text)
+        self.statusBar().showMessage(self.Ver)
+        
 
     # 初始化
     def init(self):
         self.setupUi(self)
-        self.textEdit.append('软件初始化成功')
+        self.write_text(' 软件初始化成功')
         self.setWindowTitle('基于特征识别的手写数字识别')
-        self.Button_Train.clicked.connect(self.run)
         self.setWindowIcon(QIcon('./logo.ico'))
+        self.Button_Train.clicked.connect(self.run)
 
     def run(self):
         self.dl.start()
 
     # Log记录
     def write_text(self, str):
-        now=datetime.now()
-        Mes=now.strftime("%H:%M:%S")+str
+        now = datetime.now()
+        Mes = now.strftime("%H:%M:%S") + str
         self.textEdit.append(Mes)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     Mc = MyClass()
+    apply_stylesheet(app, theme='dark_purple.xml')
     Mc.show()
     sys.exit(app.exec_())
