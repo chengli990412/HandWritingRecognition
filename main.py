48d854830499263f9113b83444a1e033fd01acdb
Cheng先生 <cheng425863@icloud.com>
2023-04-07 14:52:45 +0800

    版本：V1.0.0.8 1.补充识别功能，训练-识别流程基本完成

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from qt_material import apply_stylesheet
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FC
 import numpy as np
+import tensorflow as tf
 
 
 class MyClass(QMainWindow, Ui_MainWindow):
-    Ver = "版本：1.0.0.7       更新日期：20230407"
+    Ver = "版本：1.0.0.8       更新日期：20230407"
 
     # 深度学习类实例化
     dl = DeepLearning()
@@ -26,6 +27,9 @@ class MyClass(QMainWindow, Ui_MainWindow):
     # 识别率函数图像Y坐标
     yacc = []
 
+    # 显示图像索引
+    image_index = 0
+
     def __init__(self, parent=None):
         super(MyClass, self).__init__(parent=parent)
         self.init()
@@ -35,6 +39,25 @@ class MyClass(QMainWindow, Ui_MainWindow):
         self.dl.drawMat.connect(self.DrawMat)
         # 刷新底部状态栏显示相关信息
         self.statusBar().showMessage(self.Ver)
+
+
+        # 这里读取图像
+        files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz',
+                 't10k-images-idx3-ubyte.gz']
+        paths = []
+        for fname in files:
+            paths.append(os.path.join('MNIST/', fname))
+        with gzip.open(paths[0], 'rb') as lbpath:
+            self.train_labels = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+        with gzip.open(paths[1], 'rb') as imgpath:
+            self.train_images = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(self.train_labels), 28, 28)
+        with gzip.open(paths[2], 'rb') as lbpath:
+            self.test_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+        with gzip.open(paths[3], 'rb') as imgpath:
+            self.test_image = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(self.test_label), 28, 28)
+
+        # 显示初始图像
+        self.ShowImage()
 
     # 初始化控件&&绑定控件事件
     def init(self):
@@ -50,11 +73,14 @@ class MyClass(QMainWindow, Ui_MainWindow):
         self.verticalLayout.addWidget(self.canvas)
         self.ax = self.fig.add_subplot(111)
 
-        # 训练开始按钮绑定
+        # 控件事件绑定
         self.Button_Train.clicked.connect(self.run)
         self.Button_LoadModel.clicked.connect(self.DrawMat)
+        self.spinBox.valueChanged.connect(self.SpinBoxValueChange)
+        self.pushButton_LoadModel.clicked.connect(self.Load_RecognizeModel)
+        self.pushButton_ImageRecognize.clicked.connect(self.RecognizeImage)
 
-        self.pushButton.clicked.connect(self.ShowImage)
+
 
     # 这里是对深度学习训练运行的调用
     def run(self):
@@ -86,30 +112,36 @@ class MyClass(QMainWindow, Ui_MainWindow):
 
     # 显示图像按钮
     def ShowImage(self):
-        files = ['train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz', 't10k-labels-idx1-ubyte.gz',
-                 't10k-images-idx3-ubyte.gz']
-        paths = []
-        for fname in files:
-            paths.append(os.path.join('MNIST/', fname))
-        if len(paths) == 0:
-            return '路径异常，请检测路径设置是否正确'
-
-        with gzip.open(paths[0], 'rb') as lbpath:
-            train_labels = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-        with gzip.open(paths[1], 'rb') as imgpath:
-            train_images = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(train_labels), 28, 28)
-
-        with gzip.open(paths[2], 'rb') as lbpath:
-            test_label = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-        with gzip.open(paths[3], 'rb') as imgpath:
-            test_image = np.frombuffer(imgpath.read(), np.uint8, offset=16).reshape(len(test_label), 28, 28)
-
-        image_index = 0
-        image = train_images[image_index]
-
+        image = self.train_images[self.image_index]
         qimage = QImage(image.data, image.shape[1], image.shape[0], QImage.Format_Grayscale8)
         scaled_image = QPixmap.fromImage(qimage).scaled(300, 300)
         self.label.setPixmap(scaled_image)
+
+    # SpinBox值改变事件，主要作用切换图像
+    def SpinBoxValueChange(self):
+        self.image_index = self.spinBox.value()
+        self.ShowImage()
+
+    # 加载训练好的模型
+    def Load_RecognizeModel(self):
+        try:
+            self.newmodel = tf.keras.models.load_model('Model/model.h5', compile=False)
+            self.write_text("模型加载成功")
+        except Exception as ex:
+            self.write_text("模型加载失败"+str(ex))
+
+    # 图形预测
+    def RecognizeImage(self):
+        # 预处理，将28*28转换成1*784
+        xx = tf.reshape(self.train_images[self.image_index], (-1, 28 * 28))
+        # 识别，这里输出的是0-9的概率分布
+        recog=self.newmodel.predict(xx)
+        # 应用 softmax 函数进行转换
+        probabilities = tf.nn.softmax(recog)
+        # 使用 argmax() 函数查找最大值的索引[在本项目中为对于的识别数字]
+        predicted_class_index = np.argmax(probabilities)
+        self.write_text("  模型识别结果为："+str(predicted_class_index))
+
 
 
 if __name__ == '__main__':
